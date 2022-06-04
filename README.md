803543e8dd22afb2d84506de937b7fb1ffd3006e
Barak Bloch <barak.bloch@gmail.com>
2022-06-04 19:09:47 +0300

    :sparkles: init work

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # simple_rss_reader
+
+As a happy customer of Alfred 4 APP, i was in a need for simplest RSS paeser.
+I decided to write the simplest one for my needs. 
+
+Then i saied to myself, why not to make it open source as a package for anyone who need it. 
+Although its 2022, and XML should be an not so great histor, i know that if i need it, i can only assume i am not the only one. 
+
+Feel free to use, fork, and learn (although it is as minimal and simple as possible)
+
+
+Dedicated to my beloved cat Cosmo and his "sister" Ginger :)
---
+++ tests/test_parser.py
@@ -0,0 +1,41 @@
+import json
+from src.exceptions import InvalidXML, RequestError
+from root import ROOT_DIR
+from src.reader import SimpleRssReader
+
+def _test_data(obj):
+    assert obj["headers"]["title"] == "RSS title"
+    assert obj["headers"]["link"] == "https://github.com/barakbl/simple_rss_reader"
+
+    assert len(obj["items"]) == 2
+    assert obj["items"][0]["title"] == "Item 1"
+    assert obj["items"][0]["link"] == "https://github.com/barakbl/simple_rss_reader?1"
+
+def test_load_from_string():
+    with open(f"{ROOT_DIR}/tests/assets/rss.xml") as f:
+        xmlstr = f.read()
+    a = SimpleRssReader(xmlstr)
+    d = a.to_dict()
+    _test_data(d)
+
+    j = a.to_json()
+    _test_data(json.loads(j))
+
+    for item in a.get_items():
+        assert len(item) == 3
+
+def test_load_from_string_invalid_xml_source():
+    ok = False
+    try:
+        SimpleRssReader("NOT AN XML")
+    except InvalidXML:
+        ok = True
+    assert ok == True
+
+def test_load_invalid_url():
+    ok = False
+    try:
+        SimpleRssReader("https://goog3123123213")
+    except RequestError:
+        ok = True
+    assert ok == True
